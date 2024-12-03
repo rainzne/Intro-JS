19a7b169f3223d4005c7ea1e03f116affe6cd0d9
Rainzne <151388511+rainzne@users.noreply.github.com>
2024-12-03 23:22:37 +0100

    Ajout de malus de zone de ralentissement

--- IntroJs/src/scripts/Malus.js
+++ IntroJs/src/scripts/Malus.js
@@ -1,3 +1,25 @@
+class Malus {
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = "purple";
+    }
+
+    draw(ctx) {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+  
+   
+    
+
+
+}
+
+
+
 //    // Dessiner les éléments ralentissement
 //    players.forEach(player => {
 //     obstacles.forEach(obstacle => {
